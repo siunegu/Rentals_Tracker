d552ed1a7cb2bd743d804f7364819241406acf7c
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-03 19:54:23 +1100

    rake assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
