137ca85fc0f380a5268c253b74a86247227a11e5
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-03-19 22:01:08 +1100

    added in the push in menu bar, in application.html.erb, application.scss dependencies, added classie.js, push_menu.js, push.scss as well as a production.rb config for .sass

---
+++ app/assets/javascripts/push_menu.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+// Push Menu
+	var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+		showLeftPush = document.getElementById( 'showLeftPush' ),
+		mainContent = document.getElementById( 'main-content' )
+		body = document.body;
+
+
+	showLeftPush.onclick = function() {
+		console.log('clicked');
+		classie.toggle( this, 'active' );
+		classie.toggle( mainContent, 'cbp-spmenu-push-toright' );
+		classie.toggle( menuLeft, 'cbp-spmenu-open' );
+		disableOther( 'showLeftPush' );
+	};
+
+
+	function disableOther( button ) {
+		if( button !== 'showLeftPush' ) {
+			classie.toggle( showLeftPush, 'disabled' );
+		}
+	}
+
+});	
